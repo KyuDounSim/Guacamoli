e1c6cf40d7e351e23e863e79fe24ce38485ecd34
kdsim <kdsim@localhost.localdomain>
2018-06-08 14:59:28 +0900

    Newly started.

--- app/src/main/java/com/example/inbro/guacamoli/MainActivity.java
+++ app/src/main/java/com/example/inbro/guacamoli/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.inbro.guacamoli;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.CallbackManager;
+
 public class MainActivity extends AppCompatActivity {
+
+    callbackManager = CallbackManager.Factory.create();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
